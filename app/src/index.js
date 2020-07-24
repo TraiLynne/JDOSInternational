e69abc5a8f523c8a77e3c969a46a61d95aa36fde
Trai Lynne <trailynne@gmail.com>
2020-07-23 19:25:27 -0600

    Import Bootstrap css files

--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router
 } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
